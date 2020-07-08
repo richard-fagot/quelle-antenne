4002293ba5e03dea5e815da3ed5feae9a16395f1
Richard Fagot <richard.fagot@gmail.com>
2020-07-08 10:14:36 +0200

    Move antenna height control on the map

--- assets/script.js
+++ assets/script.js
@@ -26,7 +26,7 @@ map.on('click', onMapClick);
 
 
 
-L.Control.AntennaHeight = L.Control.extend({
+L.Control.SearchRadius = L.Control.extend({
     onAdd: function(map) {
         var container = L.DomUtil.create('div', 'mapControl');
         var label = L.DomUtil.create('label', '', container);
@@ -58,11 +58,50 @@ L.Control.AntennaHeight = L.Control.extend({
     }
 });
 
+L.control.searchRadius = function(opts) {
+    return new L.Control.SearchRadius(opts);
+}
+
+L.control.searchRadius({ position: 'topright' }).addTo(map);
+
+
+L.Control.AntennaHeight = L.Control.extend({
+    onAdd: function(map) {
+        var container = L.DomUtil.create('div', 'mapControl');
+        var label = L.DomUtil.create('label', '', container);
+        label.for = 'hauteur';
+        label.innerHTML = '<center><b>Hauteur d\'installation de l\'antenne<br/> en mètre.</b></center>';
+        var height = L.DomUtil.create('input', '', container);
+        height.type = 'number';
+        height.id = 'hauteur';
+        height.value = 500;
+       
+
+      
+        L.DomEvent.on(height, 'mousedown mouseup click touchstart dblclick', L.DomEvent.stopPropagation);
+        //div.innerHTML = '<input id="rayon" type="range" min="500" max="30000" step="500" />'
+       // L.DomEvent.on(document.getElementById('rayon'), 'change', function(e) {
+        //    console.log('toto')
+        //});
+        return container;
+    },
+
+    onRemove: function(map) {
+        L.DomEvent.off(height, 'mousedown mouseup click touchstart dblclick', L.DomEvent.stopPropagation);
+        //L.DomEvent.off(document.getElementById('rayon'), 'change', function(e) {
+          //  console.log('toto')
+        //});
+    }
+});
+
 L.control.antennaHeight = function(opts) {
     return new L.Control.AntennaHeight(opts);
 }
 
 L.control.antennaHeight({ position: 'topright' }).addTo(map);
+
+
+
 //
 // Global variables
 //
@@ -160,6 +199,10 @@ function onMapClick(e) {
         
     );
 };
+
+function createSearchRadiusControl() {
+
+}
 
 function incProgressBar() {
     progress++;
